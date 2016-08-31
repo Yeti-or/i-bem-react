e79bbae97f9ad7838cd60f2efca22e62f737b2f3
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2016-08-31 21:42:30 +0300

    wip

--- tests/test.js
+++ tests/test.js
@@ -3,10 +3,30 @@ var expect = require('chai').expect;
 var React = require('react');
 var BEM = require('..');
 
-var ReactComponent = require('./component.js');
+var ReactComponent = require('./component.js').default;
 
 BEM.registerComponent('component', ReactComponent);
 
-it('should', () => {
-    expect('true').to.eql('true');
+describe('simple', () => {
+
+    var component;
+
+    beforeEach(() => {
+        debugger
+        component = new BEM({block: 'component'});
+    });
+
+    it('should', () => {
+        expect('true').to.eql('true');
+    });
+
+    it('should getMod', () => {
+        expect(component.hasMod('hovered')).to.be.false;
+    });
+
+    it('should getMod', () => {
+        component.setMod('hovered');
+        expect(component.hasMod('hovered')).to.be.true;
+    });
+
 });
